11e9583934a784e9ccd260aea29b1087338f2f1c
MarinaPopova91 <88931948+MarinaPopova91@users.noreply.github.com>
2024-07-22 12:45:24 +0300

    Реализация заданий с массивами

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,150 +1,143 @@
 package org.example;
 
+import java.util.Random;
+
 /**
  * Класс Main
  */
 public class App {
-
     /**
-     * Функция поиска минимума среди трех чисел
+     * Функция печати массива
      *
-     * @param a первое число
-     * @param b второе число
-     * @param c третье число
-     * @return минимальное из трех чисел
+     * @param array массив
+     * @return массив в виде строки
      */
-    private static Integer min(Integer a, Integer b, Integer c) {
-        Integer result;
-        if (a <= b && a <= c) {
-            return a;
-        } else if (b <= c) {
-            return b;
-        } else {
-            return c;
+    private static void printArray(int[] array) {
+        String str = "(";
+        for (int i = 0; i < array.length; i++) {
+            str = str + array[i] + " ";
         }
+        str = str.trim() + ")";
+        System.out.println(str);
     }
 
     /**
-     * Функция определения високосного года
+     * Функция генерации массива
      *
-     * @param year год
-     * @return год является/не является високосным
+     * @param length длина массива
+     * @param max    максимльный элемент массива
+     * @param min    минимальный элемент массива
+     * @return сгенерированный массив
      */
-    public static String leapYear(Integer year) {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            return  String.format("%d год является високосным", year);
-        } else {
-            return  String.format("%d год не является високосным", year);
+    public static int[] rndArray(Integer length, Integer max, Integer min) {
+        int[] array = new int[length];
+        Random rnd = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = min + rnd.nextInt((max - min) + 1);
         }
+        return array;
     }
 
     /**
-     * *Вычисление значения функции (4*(x - 5)^2 + 7*x – 10)/(x^4+1)
+     * Функция пузырьковой сортировки массива
      *
-     * @param x
-     * @return значение функции (4*(x - 5)^2 + 7*x – 10)/(x^4+1)
+     * @param array массив
      */
-    public static Double func(Integer x) {
-        Double res = ((4 * Math.pow((x - 5), 2) + 7 * x - 10)) / (Math.pow(x, 4) + 1);
-        return res;
-    }
-
-    /**
-     * Функция определения четверти
-     *
-     * @param x значение по оси абсцисс
-     * @param y значение по оси ординат
-     * @return номер четверти
-     */
-    public static String quadrant(Integer x, Integer y) {
-        String res;
-        if (x < 0) {
-            if (y > 0) {
-                res = "II четверть";
-            } else {
-                res = "III четверть";
-            }
-        } else {
-            if (y > 0) {
-                res = "I четверть";
-            } else {
-                res = "IV четверть";
-            }
-        }
-        return res;
-    }
-
-    /**
-     * Функция определения существования треугольника
-     *
-     * @param a первая сторона треугольника
-     * @param b вторая сторона треугольника
-     * @param c третья сторона треугольника
-     * @return треугольник существует/не существует
-     */
-    public static String triangle(Integer a, Integer b, Integer c) {
-        if (a + b > c && a + c > b && b + c > a) {
-            return String.format("треугольник со сторонами (%d; %d; %d) существует" , a, b, c);
-        } else {
-            return String.format("треугольник со сторонами (%d; %d; %d) не существует" , a, b, c);
-        }
-    }
-
-    /**
-     * Решение квадратного уравнения a*x^2 + b*x + c = 0
-     *
-     * @param a
-     * @param b
-     * @param c
-     * @return корни уравнения
-     */
-    public static String equation(Integer a, Integer b, Integer c) {
-        Double d;
-        d = Math.pow(b, 2) - (4 * a * c);
-        if (d < 0) {
-            return "Корней нет";
-        } else if (d == 0) {
-            return "Уравнение имеет один корень : " + (-b) / (2 * a);
-        } else {
-            return String.format("Дискриминант > 0, уравнение имеет два корня : x1 = %.2f  x2 = %.2f" , ((-b) + Math.sqrt(d)) / (2 * a) , ((-b) - Math.sqrt(d)) / (2 * a));
-        }
-    }
-
-    /**
-     * Функция упорядочивания трёх чисел
-     *
-     * @param a первое число
-     * @param b второе число
-     * @param c третье число
-     * @return Числа в порядке возрастания
-     */
-    public static String numOrder(Integer a, Integer b, Integer c) {
+    public static void bubleSort(int[] array) {
         Integer x;
-        if (a > c) {
-            x = a;
-            a = c;
-            c = x;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j + 1] < array[j]) {
+                    x = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = x;
+                }
+            }
         }
-        if (a > b) {
-            x = a;
-            a = b;
-            b = x;
+        printArray(array);
+    }
+
+    /**
+     * Функция поиска суммы элементов массива
+     *
+     * @param array массив, сумму элементов которого необходимо вычислить
+     * @return вычисленная сумма
+     */
+    public static Integer sumArray(int[] array) {
+        Integer a = 0;
+        for (int i = 0; i < array.length; i++) {
+            a = a + array[i];
         }
-        if (b > c) {
-            x = b;
-            b = c;
-            c = x;
+        return a;
+    }
+
+    /**
+     * Функция поиска чётных элементов массива
+     *
+     * @param array введенный массив
+     * @return чётные элементы массива
+     */
+    public static String evenNumbers(int[] array) {
+        String str = "";
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                str = str + array[i] + " ";
+            } else {
+                continue;
+            }
         }
-        return String.format("Числа в порядке возрастания: %d, %d, %d ", a, b, c);
+        if (str == "") {
+            return "У массива нет четных элементов";
+        } else {
+            return String.format("Чётные элементы массива: %s", str.trim());
+        }
+    }
+
+    /**
+     * Функция удаления элемента массива по индексу
+     *
+     * @param array исходный массив
+     */
+    public static void remove(int[] array, int idx) {
+        int[] newArray = new int[array.length - 1];
+        for (int i = 0; i < array.length; i++) {
+            if (i != idx) {
+                int newIndex;
+                if (i < idx) {
+                    newIndex = i;
+                } else {
+                    newIndex = i - 1;
+                }
+                newArray[newIndex] = array[i];
+            }
+        }
+        printArray(newArray);
+    }
+
+    /**
+     * Функция изменения порядка следования элементов массива
+     *
+     * @param array исходный массив
+     */
+    public static void reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        printArray(array);
     }
 
     public static void main(String[] args) {
-        System.out.println(min(0, 3, -1));
-        System.out.println(leapYear(2024));
-        System.out.println(quadrant(3, -5));
-        System.out.println(triangle(1, 2, 100));
-        System.out.println(equation(1, 3, -4));
-        System.out.println(String.format("Значение функции равно %.1f", func(1)));
-        System.out.println(numOrder(34, 1, 11));
+        int[] array = new int[3];
+        array = rndArray(10, 30, 7);
+        printArray(array);
+        bubleSort(array);
+        System.out.println(String.format("Сумма элементов массива равна: %d", sumArray(array)));
+        System.out.println(evenNumbers(array));
+        remove(array, 1);
+        reverseArray(array);
+
+
     }
 }
