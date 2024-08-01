280fbb21d74b2653416991059ae2214d66f648a6
MarinaPopova91 <88931948+MarinaPopova91@users.noreply.github.com>
2024-08-01 17:59:28 +0300

    Из класса Foursquare убрала переменную b

--- src/main/java/shape/Foursquare.java
+++ src/main/java/shape/Foursquare.java
@@ -2,15 +2,9 @@ package shape;
 
 public class Foursquare extends Shape {
     public Double a;
-    public Double b;
 
     public Foursquare(Double a) {
         this.a = a;
-    }
-
-    public Foursquare(Double a, Double b) {
-        this.a = a;
-        this.b = b;
     }
 
     public Foursquare() {
