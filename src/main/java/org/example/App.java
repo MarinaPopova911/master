fc7848303726ec0805337a27469fa89d1712df68
MarinaPopova91 <88931948+MarinaPopova91@users.noreply.github.com>
2024-09-13 20:42:08 +0300

    Реализация заданий 19 урока

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,19 @@
 package org.example;
 
+import entity.Calculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 /**
  * Logger
- *
  */
-public class App 
-{
+public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
-    public static void main( String[] args )
-    {
-        logger.info("");
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        logger.info(String.valueOf(calculator.addition(1, 2)));
+        logger.info(String.valueOf(calculator.multiplication(1.6, 2.4)));
+        logger.info(String.valueOf(calculator.subtraction(3, 5)));
     }
 }
