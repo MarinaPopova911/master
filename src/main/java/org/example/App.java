2735d2f1e3e63be26d6ba89dfd9e49479325ace5
MarinaPopova91 <88931948+MarinaPopova91@users.noreply.github.com>
2024-09-20 20:43:43 +0300

    Реализация заданий 21 урока

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,15 +2,31 @@ package org.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * Logger
- *
+ * Regular Expressions
  */
-public class App 
-{
+public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
-    public static void main( String[] args )
-    {
-        logger.info("");
+
+    public static void main(String[] args) {
+        String str1 = "А001ТТ";
+        String regex = "^[АВЕКМНОРСТУХ]\\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}";
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(str1);
+        if (matcher.find()) {
+            logger.info(matcher.group());
+        }
+
+        String str2 = "ivanov_23@mail.COM";
+        String regex2 = "^[A-Za-z]+_[0-9]+@{1}[a-z]{3,5}\\.[A-Z]{2,3}";
+        Pattern compile2 = Pattern.compile(regex2);
+        Matcher matcher2 = compile2.matcher(str2);
+        if (matcher2.find()) {
+            logger.info(matcher2.group());
+        }
     }
 }
